2e3fd6be2c5fb53251b05962e9ea58cc8c4aca9a
rsteed11 <rsteed11@gmail.com>
2017-01-19 14:38:33 -0500

    Index prod adjustments.

--- publicWww/index.php
+++ publicWww/index.php
@@ -17,7 +17,7 @@ include_once('PoliScrape-config.php');
 
 <body>
     <?php include_once("PoliScrape-nav.php") ?>
-
+    </br>
     <!-- Page Content -->
     <div class="container">
 
@@ -33,11 +33,11 @@ include_once('PoliScrape-config.php');
             <div class="col-lg-12">
         <header class="jumbotron hero-spacer">
 
-		<p> Facilitating research and discovery with a comprehensive resource for foreign policy archival analysis. </br> <b>PoliScrape</b> is an web archive scraper that collects, searches and analyzes unredacted foreign policy documents.</p>
+		<p> Facilitating research and discovery with a comprehensive resource for foreign policy archival analysis, </br> <b>PoliScrape</b> is an web archive scraper intended to collect, search and analyze foreign policy documents.</p>
 		<p>When you find something that can be improved, please <a href="https://github.com/ntbrock/iwphys/issues">open a new github issue.</a></p>
         </br>
         <p align="center"><input type="text" name="search" placeholder="Search PoliScrape..."></p>
-</header>
+        </header>
             </div>
         </div>
 
@@ -54,10 +54,11 @@ include_once('PoliScrape-config.php');
             		    PoliScrape is made possible in part by these wonderful projects: 
             		    <a href="https://scrapy.org/">Scrapy</a> /  
             		    <a href="http://fontawesome.io/">Font Awesome 4.6.3</a> / 
-            		    <a href="https://startbootstrap.com/template-overviews/heroic-features/">HTML 5 Template is StartBootstrap.com, Heroic Features</a>
+            		    <a href="https://startbootstrap.com/template-overviews/heroic-features/">HTML 5 Template is StartBootstrap.com, Heroic Features</a> / 
+                        <a href="http://cloudapps.unc.edu/">Carolina Cloud Apps</a>
             		</p>
-        		    <p>Acknowledgement to Dr. Timothy McKeown at the University of North Carolina at Chapel Hill for the inspiration and sponsorship of PoliScrape.</p>
-                    <p>Author: Ryan Steed, 2016</p>
+        		    <p>Special acknowledgement to Dr. Timothy McKeown at the University of North Carolina at Chapel Hill for the inspiration and sponsorship of PoliScrape, and thanks to the UNC Research Computing Department for VCL computing and storage privileges.</p>
+                    <p>Author: Ryan Steed, 2017</p>
                 </div>
             </div>
         </footer>
